cbc5893f1e4083b4ce489f21aaf178d0f3b4e35a
Garvit Agrawal <garvitg369@gmail.com>
2026-08-25 23:50:53 +0530

    fixed locate by orinting all matching paths

--- mini-project1/c-shell/src/cmds_locate.c
+++ mini-project1/c-shell/src/cmds_locate.c
@@ -39,31 +39,45 @@ void locate(tknll *head, char *homwd, char *prevwd) {
                 fnd=1;
         }
 
-        if(!fnd) {
-            char *path_env=getenv("PATH");
-            if(path_env!=NULL) {
-                char *path_dup=strdup(path_env);
-                char *dir=strtok(path_dup, ":");
+        char *path_env=getenv("PATH");
+        if(path_env!=NULL) {
+            char *path_dup=strdup(path_env);
+            char *dir=strtok(path_dup, ":");
 
-                while(dir!=NULL) {
+            char *seen[500];
+            int seen_cnt=0;
+
+            while(dir!=NULL) {
+                char resolved_dir[MAXPATHLEN+5];
+                int skip=0;
+                if(realpath(dir, resolved_dir)!=NULL) {
+                    for(int i=0;i<seen_cnt;i++) {
+                        if(strcmp(seen[i], resolved_dir)==0) { skip=1; break; }
+                    }
+                    if(!skip) seen[seen_cnt++]=strdup(resolved_dir);
+                }
+
+                if(!skip) {
                     if(dir[0]=='/') snprintf(full_path, sizeof(full_path), "%s/%s", dir, tgt);
-                    
                     else snprintf(full_path, sizeof(full_path), "%s/%s/%s", cwd, dir, tgt);
-                    
 
                     DIR *d_check=opendir(full_path);
                     if(d_check!=NULL) closedir(d_check);
 
                     else if(access(full_path, X_OK)==0) {
+                        char resolved_path[MAXPATHLEN+5];
+                        if(realpath(full_path, resolved_path)!=NULL)
+                            printf("%s\n", resolved_path);
+                        else
                             printf("%s\n", full_path);
-                            fnd=1;
-                            break;
+                        fnd=1;
                     }
-                    
-                    dir=strtok(NULL, ":");
                 }
-                free(path_dup);
+
+                dir=strtok(NULL, ":");
             }
+            for(int i=0;i<seen_cnt;i++) free(seen[i]);
+            free(path_dup);
         }
 
         if(fnd==0) printf("locate: command not found (%s)\n", tgt);
